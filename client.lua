b961ee907e447900d1f1975c7f72bc3d11cbf799
w00pi <eirik.vk@hotmail.com>
2019-05-14 15:17:52 +0200

    Fixed and added some fronend stuff

--- client.lua
+++ client.lua
@@ -6,7 +6,17 @@ Citizen.CreateThread(function()
         if(IsControlJustPressed(0, 318)) then
             toggled = not toggled;
             SendNUIMessage({showing = toggled});
-            
         end
     end
+end)
+
+
+-- Add inventory item event
+RegisterNetEvent("inventory:add_and_remove") 
+AddEventHandler("inventory:add_and_remove", function(_image, _text, _add_and_remove)
+    SendNUIMessage({
+        image = _image,
+        text = _text,
+        add_and_remove = _add_and_remove
+    });
 end)
--- ui/app.js
+++ ui/app.js
@@ -1,14 +1,25 @@
+// Variables
+var count = 0
+var max = 20
+var low = 0
+
+var images = 
+{
+    ["burger"] : "pictures/bacon-burger.png",
+    ["coca-cola"] : "pictures/coca-cola.png",
+}
+
 $(function(){
 
     $("body").hide();
-    
     
     // Dragable item Code
     $(".data-item").draggable();
     
     // Removing a object
     $("#drop").droppable({
-        drop : function(event, ui){
+        drop : function(event, ui)
+        {
             ui.draggable.remove();
         }
     })
@@ -20,10 +31,41 @@ $(function(){
         if(event.data.showing == true)
         {
             $("body").fadeIn(500);
+            
+            
+            
         }
         else if(event.data.showing == false) 
         {
             $("body").fadeOut(500);
+        }
+        else if(event.data.add_and_remove = true)
+        {
+            if (count <= max)
+            {
+                count += 1;
+                console.log("here still")
+                $(".data-container").append("<div class='data-item'><img class='image' src='" + images[event.data.image] + "'/><div class='container'><p>"+ event.data.text +"</p></div></div>");
+            }
+            else
+            {
+                console.log("Inventory is full!");
+            }
+        }
+        else if(event.data.add_and_remove = false)
+        {
+            if (count >= low)
+            {
+                count -= 1;
+            }
+            else
+            {
+                console.log("Inventory is empty!");
+            }
+        }
+        else 
+        {
+            print("Invalid Syntax at removing or adding inventory items.") 
         }
     })
 });
